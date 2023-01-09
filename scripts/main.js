49daf92dcd6811118fdcf53bd8cd6dbdab0154eb
juan-zv <juanzuritavasquez@gmail.com>
2023-01-09 13:47:54 -0700

    added dark theme?

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ let year = date.getFullYear();
 
 console.log(year)
 
-document.querySelector('h1').innerHTML = '&copy;' + year;
+document.querySelector('h1').innerHTML = '&copy;' + year + " Juan Zurita - Idaho";
 
 let currentDate = document.lastModified;
 
