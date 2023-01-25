1c28da6d5ea662a559a54f8007a860547e616f1e
juan-zv <juanzuritavasquez@gmail.com>
2023-01-25 13:32:36 -0700

    did ham menu button

--- practice/week4.js
+++ practice/week4.js
@@ -5,3 +5,12 @@ function show() {
 }
 
 button.addEventListener("click", show);
+
+
+const mainnav = document.querySelector(".navigation");
+const hambutton = document.querySelector(".ham");
+
+
+hambutton.addEventListener("click", () => {
+    mainnav.classList.toggle("responsive");
+})
