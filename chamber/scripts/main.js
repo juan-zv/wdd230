38f29553782feaf731b19a2ea58fca1cb6b19787
juan-zv <juanzuritavasquez@gmail.com>
2023-01-28 19:28:02 -0700

    tried hard in web dev

---
+++ chamber/scripts/main.js
@@ -0,0 +1,7 @@
+const mainnav = document.querySelector(".navigation");
+const hambutton = document.querySelector(".ham");
+
+
+hambutton.addEventListener("click", () => {
+    mainnav.classList.toggle("responsive");
+})
