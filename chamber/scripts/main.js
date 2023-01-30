68f13a538cf7b053213980893be2ce61b1f8aaac
juan-zv <juanzuritavasquez@gmail.com>
2023-01-30 01:33:19 -0700

    minor fixes

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,3 +1,19 @@
+let date = new Date();
+let year = date.getFullYear();
+let formated_date = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) 
+
+
+
+document.querySelector('#year-and-name').innerHTML = '&copy;' + year + " Juan Zurita - Idaho";
+
+let currentDate = document.lastModified;
+
+document.querySelector('#last-modified').textContent = `Last Updated: ${currentDate}`;
+
+document.querySelector("#date").textContent = formated_date;
+
+
+
 const mainnav = document.querySelector(".navigation");
 const hambutton = document.querySelector(".ham");
 
