8c432340467d36cc622bd54210264d318e3022e6
juan-zv <juanzuritavasquez@gmail.com>
2023-02-09 12:51:05 -0700

    edited javascript for  weather widget

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -21,3 +21,16 @@ const hambutton = document.querySelector(".ham");
 hambutton.addEventListener("click", () => {
     mainnav.classList.toggle("responsive");
 })
+
+
+
+// THIS IS FOR THE RESPONSIVE WEATHER DIV
+
+const temp = document.querySelector(".t").textContent;
+const windspeed = document.querySelector(".ws").textContent;
+let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+
+if (temp <= 50 && windspeed > 3) {
+    let windchill = document.querySelector(".wc");
+    windchill.textContent = chill;
+}
