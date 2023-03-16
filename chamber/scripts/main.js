c9abfbffec00cb51c56ff48eef647ff94b3e276a
juan-zv <juanzuritavasquez@gmail.com>
2023-03-16 09:16:53 -0600

    updated chamber webpage

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,57 +1,98 @@
+//Last Modified-----------------------------------------------------------
+let currentDate = document.lastModified;
+document.getElementById('lastModified').innerHTML = `Last Modified: ${currentDate}`;
+
+
+//Copyright Date-----------------------------------------------------
 let date = new Date();
 let year = date.getFullYear();
-let formated_date = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) 
+document.querySelector('#copyrightYear').innerHTML = year;
 
 
+//Today's Date--------------------------------------------------------------
+let formated_date = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
+document.getElementById("date").innerHTML = formated_date;
 
-document.querySelector('#year-and-name').innerHTML = '&copy;' + year + " Juan Zurita - Idaho";
+//Discover Page Days Visited-----------------------------------------------------
+const visitsDisplay = document.querySelector("#visits");
 
-let currentDate = document.lastModified;
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
 
-document.querySelector('#last-modified').textContent = `Last Modified: ${currentDate}`;
-
-document.querySelector("#date").textContent = formated_date;
-
-
-
-const mainnav = document.querySelector(".navigation");
-const hambutton = document.querySelector(".ham");
-
-
-hambutton.addEventListener("click", () => {
-    mainnav.classList.toggle("responsive");
-})
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit, Welcome!`;
+}
+numVisits++;
+localStorage.setItem("visits-ls", numVisits);
 
 
+//Tuesday and Thursday Banner-----------------------------------------------
+let banner = document.getElementById("banner");
+let bannerButton = document.querySelector("#banner button");
 
-// THIS IS FOR THE RESPONSIVE WEATHER DIV
-
-const temp = document.querySelector(".t").textContent;
-const windspeed = document.querySelector(".ws").textContent;
-let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
-
-if (temp <= 50 && windspeed > 3) {
-    let windchill = document.querySelector(".wc");
-    windchill.textContent = chill;
+if (date.getDay() == 1 || date.getDay() == 2) {
+    banner.style.display = "flex";
+}
+else {
+    banner.style.display = "none";
 }
 
+bannerButton.addEventListener("click", ()=> {
+    banner.style.display = "none";
+});
 
-//storage local and session FOR DISCOVER PAGE
 
-localStorage.setItem("name", "Julie Anderson - permanent");
+// Hamburger Menu---------------------------------------------
+let hamburger = document.getElementById('hamburger');
+let menu = document.getElementById('menu');
 
-sessionStorage.setItem("class", "WDD230 - not permanent");
 
-// EJEMPLO PARA EL IF
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle ='hide';
 
-// if (!localStorage.getItem("lastVisit")) {
-//     localStorage.setItem("lastVisit", Date.now())
-//     h2 -> display "Welcome!"
-// }
-// else {
-//     let currentDate = Date.now()
+});
 
-//     let diff = (currentDate - lastDate )
 
-//     display diff -> days since visited
-// }
+//Lazy load---------------------------------------------------------------------
+const images = document.querySelectorAll('img');
+const options = { threshold: 0.5, rootMargin: '0px 0px -100px 0px' };
+
+const preloadImage = (img) => {
+  const src = img.getAttribute('data-src');
+  if (!src) {
+    return;
+  }
+  img.src = src;
+}
+
+const io = new IntersectionObserver((entries, io) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            io.unobserve(entry.target);
+        }
+    });
+}, options);
+
+images.forEach(image => {
+    io.observe(image);
+});
+
+
+//Thank you pop up message--------------------------------------------------------
+let submitForm = document.getElementById("submit");
+
+submitForm.onsubmit = () => {window.open("index.html")};
+
+
+//FETCH DATA--------------------------------------------------------------------------
+let url = "https://juan-zv.github.io/wdd230/data.json";
+
+let response = fetch(url);
+
+let data = response.json();
+
+console.log(data);
