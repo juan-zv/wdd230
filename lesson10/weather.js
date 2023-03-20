f86e6d488461e95057c421def9c8e53fec78db6f
juan-zv <juanzuritavasquez@gmail.com>
2023-03-19 20:24:10 -0600

    finished weather API

--- lesson10/weather.js
+++ lesson10/weather.js
@@ -5,6 +5,11 @@ const getWeather = async () => {
     console.log(data);
     let t = data.currentConditions.temp;
     document.querySelector('#t').textContent = Math.round(t * 5/9 +32 );
+    let image = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/${data.currentConditions.icon}.svg`;
+document.querySelector('#ws').textContent = data.currentConditions.windspeed;
+document.querySelector('#condition').textContent =  data.currentConditions.conditions;
+document.querySelector('#weather_icon').src = image;
+document.querySelector('#weather_icon').alt= data.currentConditions.conditions + ' icon';
 };
 
 getWeather();
