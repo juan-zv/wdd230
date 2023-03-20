960bd0a4720fe07f2f390d92210a5addbf0f82d6
juan-zv <juanzuritavasquez@gmail.com>
2023-03-20 13:31:45 -0600

    updaed pages

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -49,36 +49,6 @@ close.addEventListener("click", ()=> {
 })
 
 
-//Lazy load---------------------------------------------------------------------
-const images = document.querySelectorAll('img');
-const options = { threshold: 0.5, rootMargin: '0px 0px -100px 0px' };
-
-const preloadImage = (img) => {
-    const src = img.getAttribute('data-src');
-    if (!src) {
-        return;
-    }
-    img.src = src;
-}
-
-const io = new IntersectionObserver((entries, io) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            io.unobserve(entry.target);
-        }
-    });
-}, options);
-
-images.forEach(image => {
-    io.observe(image);
-});
-
-
-
-
 //FETCH DATA--------------------------------------------------------------------------
 let url = "https://juan-zv.github.io/wdd230/chamber/data.json";
 
